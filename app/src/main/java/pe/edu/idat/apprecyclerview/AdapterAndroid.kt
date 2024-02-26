474c56ea23a51b798243f88d02957cbf16d04496
Aleibwyn <mf-a@outlook.com>
2024-02-26 14:34:33 -0500

    RecyclerView - Finalizado

--- app/src/main/java/pe/edu/idat/apprecyclerview/AdapterAndroid.kt
+++ app/src/main/java/pe/edu/idat/apprecyclerview/AdapterAndroid.kt
@@ -1,14 +1,17 @@
 package pe.edu.idat.apprecyclerview
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import pe.edu.idat.apprecyclerview.databinding.ItemAndroidBinding
 
-class AdapterAndroid(val listAndroid: ArrayList<Android>) :
+class AdapterAndroid(val listAndroid: List<Android>, val context: Context) :
     RecyclerView.Adapter<AdapterAndroid.ViewHolder>() {
-        inner class ViewHolder(val binding: ItemAndroidBinding): RecyclerView.ViewHolder(binding.root)
+    inner class ViewHolder(val binding: ItemAndroidBinding) : RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val binding = ItemAndroidBinding.inflate(LayoutInflater.from(parent.context), parent, false)
@@ -16,10 +19,19 @@ class AdapterAndroid(val listAndroid: ArrayList<Android>) :
     }
 
     override fun getItemCount(): Int = listAndroid.size
+
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.binding.tvNomVersion.text = listAndroid[position].nombre
-        holder.binding.tvNumVersion.text = listAndroid[position].version
-        //holder.binding.ivAndroid =  listImages[position]
+        with(holder.binding) {
+            with(listAndroid[position]) {
+                tvNomVersion.text = nombre
+                tvNumVersion.text = version
+                ivAndroid.setImageResource(img)
+                cvAndroid.setOnClickListener(View.OnClickListener {
+                    Toast.makeText(root.context, "Click Item $nombre", Toast.LENGTH_SHORT).show()
+                })
+            }
+        }
+
     }
 
 }
--- app/src/main/java/pe/edu/idat/apprecyclerview/MainActivity.kt
+++ app/src/main/java/pe/edu/idat/apprecyclerview/MainActivity.kt
@@ -2,10 +2,33 @@ package pe.edu.idat.apprecyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import pe.edu.idat.apprecyclerview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding =  ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.rvAndroid.layoutManager = LinearLayoutManager(this)
+        binding.rvAndroid.adapter = AdapterAndroid(listAndroid(),this)
+    }
+
+    private fun listAndroid(): List<Android> {
+        return listOf(
+            Android("Apple Cake", "Android 1.0", R.drawable.image1),
+            Android("Petit Four", "Android 1.1", R.drawable.image2),
+            Android("Cupcake", "Android 1.5", R.drawable.image3),
+            Android("Donut", "Android 1.6 ", R.drawable.image4),
+            Android("Eclair", "Android 2.0 - 2.1", R.drawable.image5),
+            Android("Froyo", "Android 2.2 - 2.2.3", R.drawable.image6),
+            Android("Gingerbread", "Android 2.3 - 2.3.7", R.drawable.image7),
+            Android("Honeycomb", "Android 3.0 - 3.2.6", R.drawable.image8),
+            Android("Jelly Bean", "Android 4.1 - 4.3.1", R.drawable.image9),
+            Android("Marshmallow", "Android 6.0 - 6.1", R.drawable.image10)
+            )
     }
 }
